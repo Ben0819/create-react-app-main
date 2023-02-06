ca5a8eae71c4c6c558b5698bc426668b4407072a
Ben0819 <116129299+Ben0819@users.noreply.github.com>
2023-02-05 23:15:19 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,5 +21,38 @@ function App() {
     </div>
   );
 }
+const function1 = function () {
+  console.log(btn);
+  // const target = document.querySelector("#card2");
+  const div1 = document.querySelector(".card");
+  var child = document.body.appendChild(div1.cloneNode(true));
+  child.classList.add('card2');
+  console.log(child);
+};
+const btn = document.getElementById("duplicate");
+btn.addEventListener("click", function1);
+
+
+const btn2 = document.getElementById("bg-color");
+btn2.addEventListener("click", function (e) {
+  console.log("i clicked button 2");
+  if (document.querySelector(".card").style.backgroundColor == "white") {
+    document.querySelector(".card").style.backgroundColor = "red";
+  } else {
+    document.querySelector(".card").style.backgroundColor = "white";
+  }
+});
+
+
+function myFunction2() {
+  document.querySelector("h4").innerHTML = "Something else";
+}
+
+document.getElementById("delete()").addEventListener('click', function(e) {
+  const card = document.querySelectorAll(".card");
+  const lastCard = card[card.length - 1];
+  lastCard.remove();
+});
+
 
 export default App;
